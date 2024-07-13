f7cad2a7f219e2f8edc4358ec7b73eabf8a353d0
kwaitsing <131563118+kwaitsing@users.noreply.github.com>
2024-07-13 16:37:53 +0800

    OK

--- index.ts
+++ index.ts
@@ -1,1 +1,35 @@
-console.log("Hello via Bun!");
+#!/usr/bin/env bun
+
+// Load env
+import { $ } from 'bun'
+import fs from 'node:fs'
+import path from 'node:path'
+import { logger } from 'toolbx'
+
+const work_dir = process.cwd()
+const assignDir = (Bun.argv.slice(2))[0]
+let scaffold = work_dir
+if (assignDir) scaffold = assignDir
+scaffold = path.resolve(scaffold)
+
+// Start ricing process
+
+if (fs.existsSync(scaffold)) {
+    if (!(fs.readdirSync(scaffold).length === 0)) { logger(`Directory not empty, abort`, 2); process.exit(1) } // Handle dir is not empty
+}
+
+
+// Git clone the spin-up template
+try {
+    await $`git clone https://github.com/kwaitsing/urn-spinup-template.git ${scaffold}`.quiet()
+    await $`cd ${scaffold} && bun install`.quiet()
+} catch (err) {
+    logger(`Something went wrong ${err}`, 2)
+}
+
+logger(`Successfully bootstrap the URN instance
+    
+cd ${scaffold}
+bun run dev
+    
+To start the dev server`, 1)
